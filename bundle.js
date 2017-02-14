933552470aeebc52935d909220dcf09702162b3e
DireCorgi <s.frank.ye@gmail.com>
2017-02-14 15:30:51 -0500

    add item mappings and category based circle sizes

--- bundle.js
+++ bundle.js
@@ -30257,11 +30257,17 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	exports.formatTime = exports.getItemName = exports.getPlayersNetWorth = exports.parsePlayers = undefined;
+	exports.formatTime = exports.getItemCategory = exports.getItemName = exports.getPlayersNetWorth = exports.parsePlayers = undefined;
 	
 	var _d = __webpack_require__(6);
 	
 	var d3 = _interopRequireWildcard(_d);
+	
+	var _items = __webpack_require__(14);
+	
+	var _items2 = _interopRequireDefault(_items);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
@@ -30301,7 +30307,11 @@
 	};
 	
 	var getItemName = exports.getItemName = function getItemName(item) {
-	  return item.replace(/_/g, ' ');
+	  return _items2.default[item].formattedName;
+	};
+	
+	var getItemCategory = exports.getItemCategory = function getItemCategory(item) {
+	  return _items2.default[item].category;
 	};
 	
 	var formatTime = exports.formatTime = function formatTime(secs) {
@@ -30478,18 +30488,39 @@
 	
 	  var yScale = d3.scaleLinear().domain([9, 0]).range([height - 80, 80]);
 	
+	  var zigZagOffset = 0;
+	
 	  data.forEach(function (purchaseLog, idx) {
 	    chart.append('line').attr('class', 'item-line').attr('x1', xOffset).attr('x2', width - 40).attr('y1', yScale(idx)).attr('y2', yScale(idx));
 	
+	    function yZigZag(d) {
+	      var zigZag = 9;
+	      var offset = 0;
+	      if (zigZagOffset === 0) {
+	        zigZagOffset = 1;
+	      } else if (zigZagOffset === 1) {
+	        offset = zigZag;
+	        zigZagOffset += 1;
+	      } else if (zigZagOffset === 2) {
+	        offset = 0;
+	        zigZagOffset += 1;
+	      } else if (zigZagOffset === 3) {
+	        offset = -zigZag;
+	        zigZagOffset = 0;
+	      }
+	      return yScale(idx) + offset;
+	    }
+	
 	    chart.selectAll('dot').data(purchaseLog).enter().append('circle').attr('class', function (d) {
+	      var category = (0, _util.getItemCategory)(d.key);
 	      if (idx < 5) {
-	        return 'item-dot radiant-dot';
+	        return 'item-dot radiant-dot ' + category + '-item-dot';
 	      } else {
-	        return 'item-dot dire-dot';
+	        return 'item-dot dire-dot ' + category + '-item-dot';
 	      }
 	    }).attr('r', 8).attr('cx', function (d) {
 	      return xScale(d.time);
-	    }).attr('cy', yScale(idx)).on('mouseover', function (d) {
+	    }).attr('cy', yZigZag).on('mouseover', function (d) {
 	      tooltip.transition().duration(150).style('opacity', 0.9);
 	      tooltip.html('<h3>' + (0, _util.getItemName)(d.key) + '</h3><h4>' + (0, _util.formatTime)(d.time) + '</h4>').attr('class', 'items-sprite-' + d.key + ' item-tooltip').style('left', d3.event.pageX + 10 + 'px').style('top', d3.event.pageY - 30 + 'px');
 	    }).on('mouseout', function (d) {
@@ -30518,6 +30549,640 @@
 	};
 	
 	exports.default = graphItemProgression;
+
+/***/ },
+/* 14 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var dotaItems = {
+	  "quelling_blade": {
+	    formattedName: 'Quelling Blade',
+	    category: 'basic'
+	  },
+	  "circlet": {
+	    formattedName: 'Circlet',
+	    category: 'basic'
+	  },
+	  "branches": {
+	    formattedName: 'Iron Branch',
+	    category: 'basic'
+	  },
+	  "boots": {
+	    formattedName: 'Boots of Speed',
+	    category: 'basic'
+	  },
+	  "sobi_mask": {
+	    formattedName: 'Sobi Mask',
+	    category: 'basic'
+	  },
+	  "slippers": {
+	    formattedName: 'Slippers of Agility',
+	    category: 'basic'
+	  },
+	  "ring_of_protection": {
+	    formattedName: 'Ring of Protection',
+	    category: 'basic'
+	  },
+	  "wraith_band": {
+	    formattedName: 'Wraith Band',
+	    category: 'common'
+	  },
+	  "ring_of_basilius": {
+	    formattedName: 'Ring of Basilius',
+	    category: 'common'
+	  },
+	  "ring_of_aquila": {
+	    formattedName: 'Ring of Aquila',
+	    category: 'common'
+	  },
+	  "tpscroll": {
+	    formattedName: 'TP Scroll',
+	    category: 'consumable'
+	  },
+	  "gauntlets": {
+	    formattedName: 'Gauntlets of Strength',
+	    category: 'basic'
+	  },
+	  "urn_of_shadows": {
+	    formattedName: 'Urn of Shadows',
+	    category: 'common'
+	  },
+	  "wind_lace": {
+	    formattedName: 'Wind Lace',
+	    category: 'basic'
+	  },
+	  "blades_of_attack": {
+	    formattedName: 'Blade of Attack',
+	    category: 'basic'
+	  },
+	  "phase_boots": {
+	    formattedName: 'Phase Boots',
+	    category: 'common'
+	  },
+	  "bracer": {
+	    formattedName: 'Bracer',
+	    category: 'common'
+	  },
+	  "ancient_janggo": {
+	    formattedName: 'Drum of Endurance',
+	    category: 'rare'
+	  },
+	  "poor_mans_shield": {
+	    formattedName: 'Poor Man\'s Shield',
+	    category: 'common'
+	  },
+	  "blade_of_alacrity": {
+	    formattedName: 'Blade of Alacrity',
+	    category: 'basic'
+	  },
+	  "boots_of_elves": {
+	    formattedName: 'Band of Elvenskin',
+	    category: 'basic'
+	  },
+	  "yasha": {
+	    formattedName: 'Yasha',
+	    category: 'rare'
+	  },
+	  "robe": {
+	    formattedName: 'Robe of the Magi',
+	    category: 'basic'
+	  },
+	  "diffusal_blade": {
+	    formattedName: 'Diffusal Blade',
+	    category: 'rare'
+	  },
+	  "bottle": {
+	    formattedName: 'Bottle',
+	    category: 'consumable'
+	  },
+	  "infused_raindrop": {
+	    formattedName: 'Infused Raindrop',
+	    category: 'basic'
+	  },
+	  "mithril_hammer": {
+	    formattedName: 'Mithril Hammer',
+	    category: 'basic'
+	  },
+	  "blight_stone": {
+	    formattedName: 'Blight Stone',
+	    category: 'basic'
+	  },
+	  "desolator": {
+	    formattedName: 'Desolator',
+	    category: 'legendary'
+	  },
+	  "claymore": {
+	    formattedName: 'Claymore',
+	    category: 'basic'
+	  },
+	  "invis_sword": {
+	    formattedName: 'Shadow Blade',
+	    category: 'rare'
+	  },
+	  "shadow_amulet": {
+	    formattedName: 'Shadow Amulet',
+	    category: 'basic'
+	  },
+	  "dust": {
+	    formattedName: 'Dust of Appearance',
+	    category: 'consumable'
+	  },
+	  "ultimate_orb": {
+	    formattedName: 'Ultimate Orb',
+	    category: 'basic'
+	  },
+	  "silver_edge": {
+	    formattedName: 'Silver Edge',
+	    category: 'legendary'
+	  },
+	  "ogre_axe": {
+	    formattedName: 'Ogre Axe',
+	    category: 'basic'
+	  },
+	  "black_king_bar": {
+	    formattedName: 'Black King Bar',
+	    category: 'legendary'
+	  },
+	  "ward_observer": {
+	    formattedName: 'Observer Ward',
+	    category: 'consumable'
+	  },
+	  "cyclone": {
+	    formattedName: 'Eul\'s Scepter of Divinity',
+	    category: 'rare'
+	  },
+	  "staff_of_wizardry": {
+	    formattedName: 'Staff of Wizardry',
+	    category: 'basic'
+	  },
+	  "void_stone": {
+	    formattedName: 'Void Stone',
+	    category: 'basic'
+	  },
+	  "ward_sentry": {
+	    formattedName: 'Sentry Ward',
+	    category: 'consumable'
+	  },
+	  "ultimate_scepter": {
+	    formattedName: 'Aghanim\'s Scepter',
+	    category: 'legendary'
+	  },
+	  "smoke_of_deceit": {
+	    formattedName: 'Smoke of Deceit',
+	    category: 'consumable'
+	  },
+	  "point_booster": {
+	    formattedName: 'Point Booster',
+	    category: 'basic'
+	  },
+	  "magic_stick": {
+	    formattedName: 'Magic Stick',
+	    category: 'basic'
+	  },
+	  "magic_wand": {
+	    formattedName: 'Magic Wand',
+	    category: 'common'
+	  },
+	  "energy_booster": {
+	    formattedName: 'Energy Booster',
+	    category: 'basic'
+	  },
+	  "arcane_boots": {
+	    formattedName: 'Arcane Boots',
+	    category: 'common'
+	  },
+	  "flying_courier": {
+	    formattedName: 'Flying Courier',
+	    category: 'consumable'
+	  },
+	  "tango": {
+	    formattedName: 'Tango',
+	    category: 'consumable'
+	  },
+	  "flask": {
+	    formattedName: 'Healing Salve',
+	    category: 'consumable'
+	  },
+	  "pers": {
+	    formattedName: 'Perseverance',
+	    category: 'basic'
+	  },
+	  "sphere": {
+	    formattedName: 'Linken\'s Sphere',
+	    category: 'legendary'
+	  },
+	  "dragon_lance": {
+	    formattedName: 'Dragon Lance',
+	    category: 'rare'
+	  },
+	  "gem": {
+	    formattedName: 'Gem of True Sight',
+	    category: 'basic'
+	  },
+	  "demon_edge": {
+	    formattedName: 'Demon Edge',
+	    category: 'basic'
+	  },
+	  "javelin": {
+	    formattedName: 'Javelin',
+	    category: 'basic'
+	  },
+	  "monkey_king_bar": {
+	    formattedName: 'Monkey King Bar',
+	    category: 'legendary'
+	  },
+	  "helm_of_iron_will": {
+	    formattedName: 'Helm of Iron Will',
+	    category: 'basic'
+	  },
+	  "mantle": {
+	    formattedName: 'Mantle of Intelligence',
+	    category: 'basic'
+	  },
+	  "null_talisman": {
+	    formattedName: 'Null Talisman',
+	    category: 'common'
+	  },
+	  "veil_of_discord": {
+	    formattedName: 'Veil of Discord',
+	    category: 'rare'
+	  },
+	  "blink": {
+	    formattedName: 'Blink Dagger',
+	    category: 'basic'
+	  },
+	  "travel_boots": {
+	    formattedName: 'Boots of Travel',
+	    category: 'rare'
+	  },
+	  "gloves": {
+	    formattedName: 'Gloves of Haste',
+	    category: 'basic'
+	  },
+	  "maelstrom": {
+	    formattedName: 'Maelstrom',
+	    category: 'rare'
+	  },
+	  "vitality_booster": {
+	    formattedName: 'Vitality Booster',
+	    category: 'basic'
+	  },
+	  "soul_booster": {
+	    formattedName: 'Soul Booster',
+	    category: 'rare'
+	  },
+	  "octarine_core": {
+	    formattedName: 'Octarine Core',
+	    category: 'legendary'
+	  },
+	  "mystic_staff": {
+	    formattedName: 'Mystic Staff',
+	    category: 'basic'
+	  },
+	  "hyperstone": {
+	    formattedName: 'Hyperstone',
+	    category: 'basic'
+	  },
+	  "mjollnir": {
+	    formattedName: 'Mjollnir',
+	    category: 'legendary'
+	  },
+	  "dagon": {
+	    formattedName: 'Dagon',
+	    category: 'rare'
+	  },
+	  "dagon_2": {
+	    formattedName: 'Dagon 2',
+	    category: 'rare'
+	  },
+	  "dagon_3": {
+	    formattedName: 'Dagon 3',
+	    category: 'legendary'
+	  },
+	  "dagon_4": {
+	    formattedName: 'Dagon 4',
+	    category: 'legendary'
+	  },
+	  "dagon_5": {
+	    formattedName: 'Dagon 5',
+	    category: 'epic'
+	  },
+	  "ring_of_health": {
+	    formattedName: 'Ring of Health',
+	    category: 'basic'
+	  },
+	  "aether_lens": {
+	    formattedName: 'Aether Lens',
+	    category: 'rare'
+	  },
+	  "ring_of_regen": {
+	    formattedName: 'Ring of Regen',
+	    category: 'basic'
+	  },
+	  "force_staff": {
+	    formattedName: 'Force Staff',
+	    category: 'rare'
+	  },
+	  "orb_of_venom": {
+	    formattedName: 'Orb of Venom',
+	    category: 'basic'
+	  },
+	  "oblivion_staff": {
+	    formattedName: 'Oblivion Staff',
+	    category: 'common'
+	  },
+	  "echo_sabre": {
+	    formattedName: 'Echo Sabre',
+	    category: 'rare'
+	  },
+	  "belt_of_strength": {
+	    formattedName: 'Belt of Strength',
+	    category: 'basic'
+	  },
+	  "basher": {
+	    formattedName: 'Skull Basher',
+	    category: 'rare'
+	  },
+	  "relic": {
+	    formattedName: 'Sacred Relic',
+	    category: 'basic'
+	  },
+	  "rapier": {
+	    formattedName: 'Divine Rapier',
+	    category: 'epic'
+	  },
+	  "vladmir": {
+	    formattedName: 'Vladimir\'s Offering',
+	    category: 'rare'
+	  },
+	  "vanguard": {
+	    formattedName: 'Vanguard',
+	    category: 'rare'
+	  },
+	  "travel_boots_2": {
+	    formattedName: 'Boots of Travel 2',
+	    category: 'legendary'
+	  },
+	  "tranquil_boots": {
+	    formattedName: 'Tranquil Boots',
+	    category: 'common'
+	  },
+	  "talisman_of_evasion": {
+	    formattedName: 'Talisman of Evasion',
+	    category: 'basic'
+	  },
+	  "stout_shield": {
+	    formattedName: 'Stout Shield',
+	    category: 'basic'
+	  },
+	  "soul_ring": {
+	    formattedName: 'Soul Ring',
+	    category: 'common'
+	  },
+	  "solar_crest": {
+	    formattedName: 'Solar Crest',
+	    category: 'rare'
+	  },
+	  "skadi": {
+	    formattedName: 'Eye of Skadi',
+	    category: 'lengendary'
+	  },
+	  "shivas_guard": {
+	    formattedName: 'Shiva\'s Guard',
+	    category: 'legendary'
+	  },
+	  "sheepstick": {
+	    formattedName: 'Scythe of Vyse',
+	    category: 'lengendary'
+	  },
+	  "satanic": {
+	    formattedName: 'Satanic',
+	    category: 'legendary'
+	  },
+	  "sange_and_yasha": {
+	    formattedName: 'Sange and Yasha',
+	    category: 'lengendary'
+	  },
+	  "sange": {
+	    formattedName: 'Sange',
+	    category: 'rare'
+	  },
+	  "rod_of_atos": {
+	    formattedName: 'Rod of Atos',
+	    category: 'rare'
+	  },
+	  "refresher": {
+	    formattedName: 'Refresher Orb',
+	    category: 'legendary'
+	  },
+	  "reaver": {
+	    formattedName: 'Reaver',
+	    category: 'basic'
+	  },
+	  "radiance": {
+	    formattedName: 'Radiance',
+	    category: 'legendary'
+	  },
+	  "quarterstaff": {
+	    formattedName: 'Quaterstaff',
+	    category: 'basic'
+	  },
+	  "power_treads": {
+	    formattedName: 'Power Treads',
+	    category: 'basic'
+	  },
+	  "platemail": {
+	    formattedName: 'Platemail',
+	    category: 'basic'
+	  },
+	  "pipe": {
+	    formattedName: 'Pipe of Insight',
+	    category: 'rare'
+	  },
+	  "orchid": {
+	    formattedName: 'Orchid Malevolence',
+	    category: 'legendary'
+	  },
+	  "necronomicon": {
+	    formattedName: 'Necronomicon',
+	    category: 'rare'
+	  },
+	  "necronomicon_2": {
+	    formattedName: 'Necronomicon 2',
+	    category: 'rare'
+	  },
+	  "necronomicon_3": {
+	    formattedName: 'Necronomicon 3',
+	    category: 'legendary'
+	  },
+	  "moon_shard": {
+	    formattedName: 'Moon Shard',
+	    category: 'legendary'
+	  },
+	  "mekansm": {
+	    formattedName: 'Mekansm',
+	    category: 'rare'
+	  },
+	  "medallion_of_courage": {
+	    formattedName: 'Medallion of Courage',
+	    category: 'common'
+	  },
+	  "mask_of_madness": {
+	    formattedName: 'Mask of Madness',
+	    category: 'common'
+	  },
+	  "manta": {
+	    formattedName: 'Manta Style',
+	    category: 'lengendary'
+	  },
+	  "lotus_orb": {
+	    formattedName: 'Lotus Orb',
+	    category: 'rare'
+	  },
+	  "lifesteal": {
+	    formattedName: 'Mask of Death',
+	    category: 'basic'
+	  },
+	  "lesser_crit": {
+	    formattedName: 'Crystalys',
+	    category: 'rare'
+	  },
+	  "iron_talon": {
+	    formattedName: 'Iron Talon',
+	    category: 'common'
+	  },
+	  "hurricane_pike": {
+	    formattedName: 'Hurricane Pike',
+	    category: 'legendary'
+	  },
+	  "hood_of_defiance": {
+	    formattedName: 'Hood of Defiance',
+	    category: 'rare'
+	  },
+	  "helm_of_the_dominator": {
+	    formattedName: 'Helm of the Dominator',
+	    category: 'rare'
+	  },
+	  "heavens_halberd": {
+	    formattedName: 'Heaven\'s Halberd',
+	    category: 'legendary'
+	  },
+	  "heart": {
+	    formattedName: 'Heart of Tarrasque',
+	    category: 'legendary'
+	  },
+	  "headdress": {
+	    formattedName: 'Headdress',
+	    category: 'common'
+	  },
+	  "hand_of_midas": {
+	    formattedName: 'Hand of Midas',
+	    category: 'rare'
+	  },
+	  "guardian_greaves": {
+	    formattedName: 'Guardian Greaves',
+	    category: 'legendary'
+	  },
+	  "greater_crit": {
+	    formattedName: 'Daedalus',
+	    category: 'legendary'
+	  },
+	  "glimmer_cape": {
+	    formattedName: 'Glimmer Cape',
+	    category: 'rare'
+	  },
+	  "ghost": {
+	    formattedName: 'Ghost Sceper',
+	    category: 'basic'
+	  },
+	  "faerie_fire": {
+	    formattedName: 'Faerie Fire',
+	    category: 'consumable'
+	  },
+	  "ethereal_blade": {
+	    formattedName: 'Ethereal Blade',
+	    category: 'legendary'
+	  },
+	  "enchanted_mango": {
+	    formattedName: 'Mango',
+	    category: 'consumable'
+	  },
+	  "eagle": {
+	    formattedName: 'Eaglesong',
+	    category: 'basic'
+	  },
+	  "diffusal_blade_2": {
+	    formattedName: 'Diffusal Blade 2',
+	    category: 'legendary'
+	  },
+	  "crimson_guard": {
+	    formattedName: 'Crimson Guard',
+	    category: 'legendary'
+	  },
+	  "cloak": {
+	    formattedName: 'Cloak',
+	    category: 'basic'
+	  },
+	  "chainmail": {
+	    formattedName: 'Chainmail',
+	    category: 'basic'
+	  },
+	  "butterfly": {
+	    formattedName: 'Butterfly',
+	    category: 'legendary'
+	  },
+	  "buckler": {
+	    formattedName: 'Buckler',
+	    category: 'common'
+	  },
+	  "broadsword": {
+	    formattedName: 'Broadsword',
+	    category: 'basic'
+	  },
+	  "bloodthorn": {
+	    formattedName: 'Bloodthorn',
+	    category: 'epic'
+	  },
+	  "bloodstone": {
+	    formattedName: 'Bloodstone',
+	    category: 'legendary'
+	  },
+	  "blade_mail": {
+	    formattedName: 'Blade Mail',
+	    category: 'rare'
+	  },
+	  "bfury": {
+	    formattedName: 'Battlefury',
+	    category: 'rare'
+	  },
+	  "assault": {
+	    formattedName: 'Assault Curiass',
+	    category: 'legendary'
+	  },
+	  "armlet": {
+	    formattedName: 'Armlet of Mordiggian',
+	    category: 'rare'
+	  },
+	  "abyssal_blade": {
+	    formattedName: 'Abyssal Blade',
+	    category: 'epic'
+	  },
+	  "clarity": {
+	    formattedName: 'Clarity Potion',
+	    category: 'consumable'
+	  },
+	  "tome_of_knowledge": {
+	    formattedName: 'Tomb of Knowledge',
+	    category: 'consumable'
+	  }
+	};
+	
+	exports.default = dotaItems;
 
 /***/ }
 /******/ ]);
